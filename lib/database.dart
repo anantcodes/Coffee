62da44feae53bcad5f909e42bff5b4f6db5eab59
Anant Kanchan <anant4265@gmail.com>
2020-07-27 11:49:29 +0530

    initial commit message

---
+++ lib/database.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService
+{
+  //collection reference
+  final CollectionReference brewCollection=Firestore.instance.collection( 'brews'); 
+}
