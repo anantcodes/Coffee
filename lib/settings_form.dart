169c43289ecdae69262d4269b06e74c92073f860
Anant Kanchan <anant4265@gmail.com>
2020-08-15 21:07:51 +0530

    initial commit message

--- lib/settings_form.dart
+++ lib/settings_form.dart
@@ -2,3 +2,24 @@ import 'package:flutter/material.dart';
 
 import 'shared/constants.dart';
 
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+
+
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+
+  // form values
+  String _currentName;
+  String _currentSugars;
+  int _currentStrength;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
