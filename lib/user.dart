6713667ce78e2062df4f496c0396790b220d025e
Anant Kanchan <anant4265@gmail.com>
2020-08-16 20:58:43 +0530

    initial commit message

--- lib/user.dart
+++ lib/user.dart
@@ -5,3 +5,13 @@ class User{
 
   User({this.uid});
 }
+
+class  UserData
+{
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+
+  UserData({ this.uid, this.sugars, this.strength, this.name });
+}
